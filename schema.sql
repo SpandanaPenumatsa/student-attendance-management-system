db150223047074e368cb5ada1c886de6a9304168
SpandanaPenumatsa <spandanapnmts@gmail.com>
2026-06-11 19:35:30 +0530

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,7 @@
 CREATE TABLE students (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
-    name TEXT NOT NULL
+    name TEXT NOT NULL,
+    roll_no TEXT,
+    course TEXT,
+    attendance INTEGER DEFAULT 0
 );
